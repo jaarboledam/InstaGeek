0f8df358036a505e253f0baf3c57b98281c18236
jaarboledam <jaarboledam@gmail.com>
2016-10-23 08:06:16 -0500

    Se finaliza endpoint para seguir a un usuario al igual que sus test

--- followers/views.py
+++ followers/views.py
@@ -1,8 +1,9 @@
-from rest_framework.mixins import ListModelMixin
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
-from followers.serializers import RelationshipUserSerializer
+from followers.models import Relationship
+from followers.serializers import RelationshipUserSerializer, RelationshipSerializer
 from followers.utils import get_following
 
 
@@ -13,3 +14,16 @@ class FollowingViewSet(ListModelMixin, GenericViewSet):
 
     def get_queryset(self):
         return get_following(self.request.user)
+
+
+class FollowViewSet(CreateModelMixin, GenericViewSet):
+
+    queryset = Relationship.objects.all()
+    serializer_class = RelationshipSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        """
+        Hacemos que el usuario autenticado sea el seguidor
+        """
+        serializer.save(origin=self.request.user)
